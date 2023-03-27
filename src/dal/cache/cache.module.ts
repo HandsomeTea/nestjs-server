3b172692ebd6e7918b5d6b9a6a1029d94f662d86
handsome tea <handsomeTea.coco@gmail.com>
2023-03-27 15:22:44 +0800

    code update

--- src/dal/cache/cache.module.ts
+++ src/dal/cache/cache.module.ts
@@ -2,29 +2,22 @@ import { Module } from '@nestjs/common';
 import { RedisModule } from 'nestjs-ioredis';
 import { cacheProvider } from './cache.providers';
 
-console.log('===============================================');
-console.log(`process.env.REDIS_URL: ${process.env.REDIS_URL}`);
-console.log('===============================================');
 @Module({
 	imports: [
-		RedisModule.forRoot((() => {
-			// const { hostname, port, password } = new URL(process.env.REDIS_URL);
-			const { hostname, port, password } = new URL('redis://127.0.0.1:6379');
+		RedisModule.forAsync({ name: 'test' }, {
+			useFactory: () => {
+				const { hostname, port, password } = new URL(process.env.REDIS_URL);
 
-			setTimeout(() => {
-				console.log('===============================================');
-				console.log(`process.env.REDIS_URL: ${process.env.REDIS_URL}`);
-				console.log('===============================================');
-			}, 10);
-			return [
-				{
-					name: 'test',
-					host: hostname,
-					port: parseInt(port),
-					password
-				}
-			];
-		})())
+				return [
+					{
+						host: hostname,
+						port: parseInt(port),
+						password
+					}
+				];
+			}
+		}
+		)
 	],
 	providers: [cacheProvider],
 	exports: [cacheProvider]
--- src/modules/health/health.module.ts
+++ src/modules/health/health.module.ts
@@ -1,27 +1,41 @@
 import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
-// import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
 // import { SequelizeModule } from '@nestjs/sequelize';
 import { HealthController } from './health.controller';
+// import { system } from '@/configs';
 
 @Module({
 	imports: [
-		TerminusModule
-		// MongooseModule.forRoot(process.env.DB_URL)
-		// SequelizeModule.forRoot((() => {
-		// 	const { hostname, port, username, password, pathname } = new URL(process.env.DB_URL);
+		TerminusModule,
+		MongooseModule.forRootAsync({
+			useFactory: () => {
+				return {
+					uri: process.env.DB_URL
+				};
+			}
+		})
+		// SequelizeModule.forRootAsync({
+		// 	useFactory: () => {
+		// 		const { hostname, port, username, password, pathname } = new URL(process.env.DB_URL);
 
-		// 	return {
-		// 		dialect: 'postgres',
-		// 		host: hostname,
-		// 		port: parseInt(port),
-		// 		username,
-		// 		password,
-		// 		database: pathname.substring(1),
-		// 		autoLoadModels: true,
-		// 		synchronize: true
-		// 	};
-		// })())
+		// 		return {
+		// 			dialect: 'postgres',
+		// 			host: hostname,
+		// 			port: parseInt(port),
+		// 			username,
+		// 			password,
+		// 			database: pathname.substring(1),
+		// 			autoLoadModels: true,
+		// 			synchronize: true,
+		// 			logging: (...args) => {
+		// 				const [sql] = args as unknown as [string, { bind: Array<string | number> | undefined }];
+
+		// 				system('sequelize-command').debug(sql);
+		// 			}
+		// 		};
+		// 	}
+		// })
 	],
 	controllers: [HealthController]
 })
